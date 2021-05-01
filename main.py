9fcff5a7b75c7287c79a8c78f22c1e4089078559
Raio Chea <>
2021-05-01 18:38:53 +0000

    fixed stuff, added css

--- main.py
+++ main.py
@@ -7,4 +7,11 @@ app = Flask(__name__)
 def index():
   return render_template("index.html")
 
+@app.route("/factorial/<number>")
+def factorial(number):
+  fact = 1
+  for i in range(1, int(number)+1):
+    fact = fact * i 
+  return str(fact)
+
 app.run(host='0.0.0.0', port=8080)
