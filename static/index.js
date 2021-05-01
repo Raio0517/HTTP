a10003f56af0b4b78e550ad3744fa81ad32918a7
Raio Chea <>
2021-05-01 18:13:09 +0000

    added static stuff

---
+++ static/index.js
@@ -0,0 +1,13 @@
+function factorial() {
+  xhr = new XMLHttpRequest();
+  xhr.open("GET", "/factorial/" + $("#factorial").val());
+  
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      $("#result").html(xhr.response);
+    }
+  }
+
+  xhr.send();
+  
+}
